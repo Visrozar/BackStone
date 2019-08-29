036ec33932a2b325c8867044c8a6fd738d845c40
elvis <elvis.rozario@freightbro.com>
2019-08-30 00:55:48 +0530

    star frequency dynamic

--- src/star.js
+++ src/star.js
@@ -4,11 +4,12 @@ export default {
 
 function getStarPositions() {
     let positions = [];
-    for (let index = 0; index < 10; index++) {
+    let star_frequency = 20;
+    for (let index = 0; index < star_frequency; index++) {
         positions.push({
             x: Math.random(),
             y: Math.random(),
-            size: Math.random()
+            size: Math.random() * 2
         })
     }
     return positions;
