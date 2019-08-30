cd9bd6bcefb50a58fcb908cb3d9e2773b360bde0
Nishad Wadwekar <nishad290@gmail.com>
2019-08-30 18:24:54 +0530

    fixed stationary objects

--- src/obstacle.js
+++ src/obstacle.js
@@ -7,6 +7,9 @@ export default class Obstacle extends Sprite.class {
 
         super();
 
+        this.width = Math.floor(Math.random() * (40 - 20 + 1) + 20);
+        this.height = Math.floor(Math.random() * (40 - 20 + 1) + 20);
+
         if (!isStationary){
             // spawn out of canvas
             //random Int from interval = Math.floor(Math.random() * (max - min) + min); // considering -10 to width + 10
@@ -18,33 +21,26 @@ export default class Obstacle extends Sprite.class {
             }
             else {
                 // in canvas , y -> -10 to 0
-                this.y = Math.floor(Math.random() * 10 - 10);
+                this.y = -this.height - 10;
             }
+
+            var availableColors = this.get_colours();
+            this.color = availableColors[Math.floor(Math.random() * availableColors.length)];
+
         }
         else{
+
+            this.width = this.width * 2;
+            this.height = this.height * 2;
+            this.color = 'white';
             // stationary object will be inside canvas
             this.x = Math.floor(Math.random() * initialValues.canvas.width);
-            this.y = Math.floor(Math.random() * 10 - 10);
+            this.y = -this.height - 10;
         }
 
         // initial velocity is zero
         this.dx = 0;
         this.dy = initialValues.backgroundSpeed;
-
-        var availableColors = this.get_colours();
-        this.color = availableColors[Math.floor(Math.random() * availableColors.length)];
-        
-        // 40 to 20
-        if(!isStationary){
-            this.width = Math.floor(Math.random() * (40 - 20 + 1) + 20);
-            this.height = Math.floor(Math.random() * (40 - 20 + 1) + 20);
-        }
-        // only for testing stationary objects
-        else{
-            this.width = Math.floor(Math.random() * (70 - 50 + 1) + 50);
-            this.height = Math.floor(Math.random() * (70 - 50 + 1) + 50);
-            this.color = 'white';
-        }
 
     };
 
