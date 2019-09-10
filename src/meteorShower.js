01a0a90a0bfda8d5b888f3e596a11d5b372ac458
elvis <elvis.rozario@freightbro.com>
2019-09-10 21:01:23 +0530

    back in meteor shower

--- src/meteorShower.js
+++ src/meteorShower.js
@@ -1,4 +1,5 @@
-import { Sprite, getContext } from "kontra";
+import { Sprite } from "kontra";
+import Rewind from './rewind';
 import initialValues from './initialValues';
 import themes from './obstacleTheme';
 
@@ -7,8 +8,8 @@ class MeteorShower {
         this.meteors = [];
         this.alive = false;
         this.spawn = false;
-        this.spawnStopAtScore = 70;
-        this.startShowerAtScore = 75;
+        this.spawnStopAtScore = 3;
+        this.startShowerAtScore = 10;
         this.stopShowerAtScore = 95;
         this.spawnStartAtScore = 99;
         this.meteor_score_check = 0;
@@ -29,8 +30,8 @@ class MeteorShower {
     }
 
     addMeteor() {
-        if (Math.random() <= 0.08) {
-            if (this.meteors.length > 10) this.meteors.shift();
+        if (Math.random() <= 0.08 && this.meteors.length < 15) {
+            // if (this.meteors.length > 10) this.meteors.shift();
             this.meteors.push(this.getMeteor());
         }
     }
@@ -38,23 +39,42 @@ class MeteorShower {
     getMeteor() {
         let width = 60;
         let height = 60;
+        let speed = 14;
         return Sprite({
             width: width,
             height: height,
 
             x: Math.random() * (initialValues.canvas.width),
-            y: -height - initialValues.canvas.height,
+            y: -height - (initialValues.canvas.height * speed / 4),
 
             dx: 0,
-            dy: 14,
+            dy: speed,
 
-            render: themes.meteor_shower
+            render: themes.meteor_shower,
+            rewind: new Rewind,
+            update: function () {
+                if (initialValues.rewindMode) {
+                    if (!this.rewind.doneRewind()) {
+                        let rewind = this.rewind.back();
+                        this.x = rewind.x;
+                        this.y = rewind.y;
+                    }
+                } else {
+                    this.rewind.add(this.x, this.y);
+                }
+                this.advance();
+            }
         })
     }
 
     update(player_sprite, loop) {
-        if (this.spawn) this.addMeteor();
-        this.meteors.forEach(function (meteor) {
+        if (this.spawn && !initialValues.rewindMode) this.addMeteor();
+        let meteors = this.meteors;
+        meteors.forEach(function (meteor, key) {
+            // if this meteor is destroyed
+            if (meteor.y > (initialValues.canvas.height * 2)) {
+                meteors.splice(key, 1);
+            }
             meteor.update();
             // check if player collided with meteor
             if (meteor.collidesWith(player_sprite)) {
