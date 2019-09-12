0911dba35da2998cd5359ba26b9a26691bf8da8c
elvis <elvis.rozario@freightbro.com>
2019-09-12 20:20:20 +0530

    fix for game start on back mode

--- src/bg.js
+++ src/bg.js
@@ -28,9 +28,11 @@ export default function bg_sprite(stars) {
             if (initialValues.rewindMode) {
                 if (this.rewind.doneRewind()) {
                     initialValues.rewindMode = false;
-                    setTimeout(function(){
-                        initialValues.spawnObstacle = true;
-                    },6000);
+                    if (initialValues.gameStart) {
+                        setTimeout(function () {
+                            initialValues.spawnObstacle = true;
+                        }, 6000);
+                    }
                 }
                 else {
                     let rewind = this.rewind.back();
